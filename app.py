b42279309e9f205557f1018353573723142643c3
Jeff Caplan <jeffrey.caplan@gmail.com>
2017-09-19 23:17:15 -0400

    Revert "Revert "Changed display formatting""
    This reverts commit efbeacceb1225b2fde6410f8f065fac0ca63f74e.

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import os
 import re
 import sys
 from threading import Thread
+import time
 import urllib.request
 
 app = Flask(__name__)
@@ -14,13 +15,12 @@ RF_TOKEN = config.get('rf_token')
 
 @app.route('/slack', methods=['POST'])
 def inbound():
-    # Parse the parameters
+    # Parse the parameters & validate Slack token
     token = request.form.get('token', None)
-    if not token:  # or token is incorrect
+    if not token:
         abort(400)
     if token != SLACK_TOKEN:
         return Response('Invalid token.'), 403
-
     queryString = request.form.get('text', None)
     response_url = request.form.get('response_url', None)
 
@@ -52,23 +52,67 @@ def inbound():
             # Prepare and send RF API query response metrics & Connect API URL to Slack
             for res in result['events']:
                 data = res['stats']['metrics']
-                metrics = "RF Metrics for " + queryString + ":\n"
-                metrics += "\tTotal Hits: " + str(data.get('totalHits')) + '\n'
-                metrics += "\tCriticality: " + str(data.get('criticality')) + '\n'
-                metrics += "\tRisk Score: " + str(data.get('riskScore')) + '\n'
-                metrics += "\tDark Web Hits: " + str(data.get('darkWebHits')) + '\n'
-                metrics += "\tSeven Day Hits: " + str(data.get('sevenDaysHits')) + '\n'
-                metrics += "\tOne Day Hits: " + str(data.get('oneDayHits'))
+                crit = data.get['criticality']
+                if(crit == 1):
+                    criticality = "Low"
+                    color = "#61C359"
+                elif(crit == 2):
+                    criticality = "Important"
+                    color = "#DFE125"
+                elif(crit == 3):
+                    criticality = "Critical"
+                    color = "#E15025"
+                totalHits = data.get('totalHits')
+                riskScore = data.get('riskScore')
+                maliciousHits = data.get('maliciousHits')
+                darkWebHits = data.get('darkWebHits')
+                sevenDayHits = data.get('7DayHits')
+                oneDayHits = data.get('1DayHits')
+
                 payload = json.dumps({
                     'attachments': [
                     {
                         'fallback': '<https://www.recordedfuture.com/live/sc/entity/' + entity + '|Recorded Future API Response.>',
+                        'color': color,
                         'pretext': 'Here are your results:',
-                        'color': '#36a64f',
-                        'title': 'Recorded Future Connect API URL',
+                        'author_name': queryString,
+                        'title': 'Recorded Future Connect API Search Results',
                         'title_link': 'https://www.recordedfuture.com/live/sc/entity/' + entity,
-                        'thumb_url': 'https://www.recordedfuture.com/assets/google-results-logo.png',
-                        'text': metrics
+                        "fields": [
+                        {
+                            "title": "Criticality",
+                            "value": criticality,
+                            "short": true
+                        },
+                        {
+                            "title": "Risk Score:",
+                            "value": riskScore,
+                            "short": true
+                        },
+                        {
+                            "title": "Malicious Hits",
+                            "value": maliciousHits,
+                            "short": true
+                        },
+                        {
+                            "title": "Dark Web Hits:",
+                            "value": darkWebHits,
+                            "short": true
+                        },
+                        {
+                            "title": "7-day Hits (Total):",
+                            "value": sevenDayHits,
+                            "short": true
+                        },
+                        {
+                            "title": "1-day Hits (Total):",
+                            "value": oneDayHits,
+                            "short": true
+                        },
+                        ],
+                        "footer": "Recorded Future",
+                        "footer_icon": "https://media.licdn.com/mpr/mpr/shrink_200_200/AAEAAQAAAAAAAAOnAAAAJDJjOGE4Mzk4LWMwMDktNGY1OC05NTNmLTBlNDVhNjllYjcxZg.png",
+                        "ts": time.time()
                     }
                     ]
                 }).encode('utf8')
@@ -76,21 +120,20 @@ def inbound():
         req = urllib.request.Request(response_url, data=payload, headers={'content-type': 'application/json'})
         response = urllib.request.urlopen(req)
 
-    # Determine if queryString is an IP address or domain name and call RF query function in another thread, so we can reply to the Slack API in time
+    # Determine if queryString is an IP address or domain name
     if re.match("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", queryString):
         entity = "ip:" + queryString
         data_group = "IpAddress"
-        t = Thread(target=queryRF)
-        t.start()
-        return "Querying Recorded Future API for IP address: " + queryString + "\nPlease wait..."
     elif re.match("^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$", queryString):  # TODO: Implement better REGEX for handling of possible domain names
         entity = "idn:" + queryString
         data_group = "InternetDomainName"
-        t = Thread(target=queryRF)
-        t.start()
-        return "Querying Recorded Future API for Domain: " + queryString + "\nPlease wait..."
     else:
-        return "Invalid query detected. Current support only includes IP addresses or domain names."
+        return "Invalid query detected. Current support only includes individual IP addresses and domain names."
+
+    # Call the RF API query function in another thread, so we can reply to Slack within 3000ms
+    t = Thread(target=queryRF)
+    t.start()
+    return "Querying Recorded Future API.  Please wait..."
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
